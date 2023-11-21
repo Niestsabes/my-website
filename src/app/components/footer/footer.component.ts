bc5a7d92f53b540bf0e2f945966381df9dd203e5
Niestsabes <sebcayet@gmail.com>
2023-11-21 22:49:22 +0100

    :construction: Imporves footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ROUTES } from 'src/app/constants/routes.dictionary';
 import { ResumeInterface } from 'src/app/models/resume.interface';
 import { DataService } from 'src/app/services/data.service';
 
@@ -9,9 +10,19 @@ import { DataService } from 'src/app/services/data.service';
 })
 export class FooterComponent {
 
+  public readonly ROUTES = ROUTES;
+
   constructor(private _data: DataService) { }
+
+  public scrollToTop() {
+    window.scrollTo(0, 0);
+  }
 
   get data(): ResumeInterface {
     return this._data.resume;
+  }
+  
+  get year(): number {
+    return new Date().getFullYear();
   }
 }
