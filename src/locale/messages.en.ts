c0e7052c8d6ae9425530ef9855e7c25d9f4d6f71
Niestsabes <sebcayet@gmail.com>
2023-11-22 21:28:13 +0100

    :arrow_up: Migrate to Angular 16

--- src/locale/messages.en.ts
+++ src/locale/messages.en.ts
@@ -7,7 +7,7 @@ const messages = {
   experiences: 'Experiences',
   firstName: 'First Name',
   footerText:
-    'This site is guaranteed 100% cookie-free! Developed by Sébastien CAYET (himself) with Angular 15.',
+    'This site is guaranteed 100% cookie-free! Developed by Sébastien CAYET (himself) with Angular 16.',
   from: 'From',
   greatings: 'Greatings!',
   greatingsDescription:
