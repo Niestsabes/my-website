737277b7dd780acd64261286dd410cc8da768965
Niestsabes <sebcayet@gmail.com>
2023-11-24 21:18:20 +0100

    :zap: Changes .png and .jpg to .webp

--- src/app/constants/images.dictionary.ts
+++ src/app/constants/images.dictionary.ts
@@ -1,5 +1,5 @@
 export const IMAGES = {
-    siteMainImage: "assets/images/avatar256.png",
-    profilImage: "assets/images/avatar.png",
-    mainPhoto: "assets/images/main-photo.jpg",
+    siteMainImage: "assets/images/avatar256.webp",
+    profilImage: "assets/images/avatar.webp",
+    mainPhoto: "assets/images/main-photo.webp",
 };
--- src/app/constants/locale.dictionary.ts
+++ src/app/constants/locale.dictionary.ts
@@ -4,21 +4,21 @@ export const LOCALES : Array<LocaleInterface> = [
     {
         id: 'en',
         name: 'English',
-        image: 'assets/images/flag-english.png'
+        image: 'assets/images/flag-english.webp'
     },
     {
         id: 'fr',
         name: 'Français',
-        image: 'assets/images/flag-french.png'
+        image: 'assets/images/flag-french.webp'
     },
     {
         id: 'es',
         name: 'Español',
-        image: 'assets/images/flag-spanish.png'
+        image: 'assets/images/flag-spanish.webp'
     },
     {
         id: 'ja',
         name: '日本語',
-        image: 'assets/images/flag-japanese.png'
+        image: 'assets/images/flag-japanese.webp'
     }
 ]
